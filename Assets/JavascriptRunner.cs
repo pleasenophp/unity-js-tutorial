27b4805cd69e12a8ee0a18ba6c0982a1e961d081
Andriy Tevelyev <ndruha@gmail.com>
2020-05-19 23:03:15 +0200

    Example of proxy class

--- Assets/JavascriptRunner.cs
+++ Assets/JavascriptRunner.cs
@@ -1,22 +1,36 @@
 ﻿using UnityEngine;
 using Jint;
 using System;
+using Jint.Runtime.Interop;
 
 public class JavascriptRunner : MonoBehaviour
 {
     private Engine engine;
-    
+
+    private class GameApi {
+      public void ApiMethod1() {
+        Debug.Log("Called api method 1");
+      }
+
+      public int ApiMethod2() {
+        Debug.Log("Called api method 2");
+        return 2;
+      }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
       engine = new Engine();
       engine.SetValue("log", new Action<object>(msg => Debug.Log(msg)));
-      engine.SetValue("myFunc", 
-        new Func<int, string>(number => "C# can see that you passed: "+number));
+
+      engine.SetValue("GameApi", TypeReference.CreateTypeReference(engine, typeof(GameApi)));
 
       engine.Execute(@"
-        var responseFromCsharp = myFunc(108);
-        log('Response from C#: '+responseFromCsharp);        
+        var gameApi = new GameApi();
+        gameApi.ApiMethod1();
+        var result = gameApi.ApiMethod2();
+        log(result);
       ");
     }
 }
