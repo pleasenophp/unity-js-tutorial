aa2fe7280bc118f5a76e87a2b299f135d490ee6c
Andriy Tevelyev <ndruha@gmail.com>
2020-05-21 00:37:46 +0200

    Modules showcase

---
+++ Game/MyModule.js
@@ -0,0 +1,7 @@
+export const myFunction1 = () => {
+  return "This is function 1 from my module";
+}
+
+export const myFunction2 = () => {
+  return "This is function 2 from my module";
+}
--- Game/index.js
+++ Game/index.js
@@ -1,6 +1,9 @@
+import { myFunction1, myFunction2 } from './MyModule';
 
 const hello = () => {
   return "Hello from JS ES6 file!";
 };
 
 log(hello());
+log(myFunction1());
+log(myFunction2());
